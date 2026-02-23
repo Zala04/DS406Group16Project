30f776b2a18d0e5c09c98ee8e5401a4a39d1bbb6
Zala04 <zaladio135@gmail.com>
2026-02-23 12:23:22 +0000

    Made a data file for reproducability purposes so everyone can use it by reading the csv

---
+++ Data.R
@@ -0,0 +1,6 @@
+#install.packages("nycflights13")
+#library(nycflights13)
+nycflights13::flights
+x <- nycflights13::flights
+#write.csv(x, "nycflights13.csv")
+nycflights13 <- read_csv("Data/nycflights13.csv")
