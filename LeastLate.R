85f27316e1bd464f47a433c63d3025f93f8a33ab
Zala04 <zaladio135@gmail.com>
2026-04-02 10:41:28 +0100

    Modelled a glm of the flights to see which carrier deviates/ is the most delayed

--- LeastLate.R
+++ LeastLate.R
@@ -1,9 +1,20 @@
 nycflights13 <- read_csv("Data/nycflights13.csv")
 library(tidyverse)
-f1 <- flights |>
+library(dplyr)
+f1 <- nycflights13 |>
   group_by(carrier) |>
-  mutate(proportionDelay = mean(dep_delay > 0 | arr_delay > 0, na.rm=TRUE)) |>
-x <- f1 |> group_by(carrier) |> count(proportionDelay)
+  summarise(proportionDelay = mean(dep_delay > 0 | arr_delay > 0, na.rm = TRUE),
+    n = n())
+f1
+x <- f1 |> group_by(carrier)
 x |> group_by(carrier) |> arrange(desc(proportionDelay))
-print(x)
-unique(airlines$carrier)
+x
+
+flights2 <- nycflights13 |>
+  mutate(delayed = dep_delay > 0 | arr_delay > 0)
+flights2$delayed <- complete.cases(flights2$delayed)
+
+mod1 <- glm(delayed ~ carrier, data = flights2,family = binomial)
+summary(model1)
+# models that are important and have a huge diff F9, FL as we are doing e^
+## next is to make a model adding some variables and see if they are needed
