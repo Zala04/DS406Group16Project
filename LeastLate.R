532405b9b20b0d822f63592677bda62644e81a9c
Zala04 <zaladio135@gmail.com>
2026-04-16 10:24:55 +0100

    facet plot for carriers  model

--- LeastLate.R
+++ LeastLate.R
@@ -1,16 +1,21 @@
 nycflights13 <- read.csv("Data/nycflights13.csv")
 library(tidyverse)
 library(dplyr)
+library(viridis)
 f1 <- nycflights13 |>
   group_by(carrier) |>
   summarise(proportionDelay = mean(dep_delay > 0 | arr_delay > 0, na.rm = TRUE),
-    n = n())
+            n = n())
 f1
 x <- f1 |> group_by(carrier) |> arrange(desc(proportionDelay))
 x
 
-x$carrier <- factor(x$carrier, levels = x$carrier)
-ggplot(x, aes(x = carrier, y= proportionDelay))+ geom_col(fill="black")
+x$carrier <- factor(x$carrier, levels = x$carrier,)
+ggplot(x, aes(x = carrier, y= proportionDelay, label = n))+ geom_col(aes(fill = n))  + coord_flip()+
+  geom_text(
+    aes(label = n),
+    hjust = 0, col="red") + ylim(0,0.8) + ggtitle("Proportion delay by carrier with number of counts associated") +
+  scale_fill_viridis_b()
 
 
 flights2 <- nycflights13 |> filter(!is.na(dep_delay), !is.na(arr_delay)) |>
@@ -32,16 +37,32 @@ anova(mod1, mod1Reduced)
 # p-val - 0.6781, model tells us that the reduced model is not siginificantly worse, can be excluded
 
 # since dep_delay is the most significant predictor with a z value of 225 we make a plot
+binomial_smooth <- function(...) {
+  geom_smooth(method = "glm", method.args = list(family = "binomial"), ...)
+}
 ggplot(flights2, aes(x = dep_delay, y = delayed)) +
   geom_jitter(alpha = 0.05) +
-  geom_smooth(method = "glm", method.args = list(family = "binomial"), color = "red") +
-    ggtitle("Delay vs Departure Delay") +
-    xlab("Departure Delay") +
-    ylab("prob of being Delayed")
+  binomial_smooth() +
+  ggtitle("Delay vs Departure Delay") +
+  xlab("Departure Delay") +
+  ylab("prob of being Delayed")
+
+# departure delay logistic regression curve
+
+#fit = glm(delayed ~ dep_delay, data=flights2, family=binomial)
+#newdat <- data.frame(dep_delay=seq(min(flights2$dep_delay), max(flights2$dep_delay),len=100))
+#newdat$delayed = predict(fit, newdata=newdat, type="response")
+#plot(delayed ~ dep_delay, data=flights2, col="red4")
+#lines(delayed ~ dep_delay, newdat, col="green4", lwd=2)
+# key plot as we figure out as dep_delay increases so does the probability of being late
 
 
-ggplot(flights2, aes(x = combinesMinutes, y = delayed)) +
-  geom_jitter(alpha = 0.05) +
-  geom_smooth(method = "glm", method.args = list(family = "binomial"), color = "red") +
-  ggtitle("Delay vs flight time")
+ggplot(flights2, aes(dep_delay, delayed)) +
+  geom_jitter(height = 0.05, alpha = 0.01) +
+  binomial_smooth() +
+  facet_wrap(~carrier, scales = "free_y") +
+  ggtitle("Probability of delay vs departure delay by each Carrier") +
+  xlab("Departure Delay") +
+  ylab("Probability of being delayed")
+
 
